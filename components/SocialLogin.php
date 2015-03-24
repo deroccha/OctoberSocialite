98cad231de288efd7fa17cd2b79db2964f1b91be
lgt <fejerferenc@Fejers-MacBook-Pro.fritz.box>
2015-03-24 19:04:21 +0100

    component refactoring

--- components/SocialLogin.php
+++ components/SocialLogin.php
@@ -5,45 +5,71 @@ use Cms\Classes\ComponentBase;
 use Auth;
 use Request;
 use Kakuki\OAuth2\Models\Setting;
-//use RainLab\User\Components\Session as RainLabSession;
-//use RainLab\User\Models\User;
+use RainLab\User\Components\Session as RainLabSession;
+use RainLab\User\Models\User;
 use Socialite;
 use Redirect;
 
 //use Kakuki\OAuth2\Classes\ProviderSession;
 
 
-class SocialLogin extends ComponentBase {
+class SocialLogin extends ComponentBase
+{
 
+    /**
+     * The request Object
+     * @object
+     */
     public    $request;
 
+    /**
+     * Requested Provider
+     * @var
+     */
     public    $provider;
 
+    /**
+     * OAuth Callback URL which matches with Page URL where Component is attached
+     * @var
+     */
     public    $callback_url;
 
+    /**
+     * @var
+     */
     public    $socialite_session;
 
+    /**
+     * All registered Socialate Provider by Plugin User
+     * @var
+     */
     public    $socialite_providers;
 
+    /**
+     * OAuth Client Secret
+     * @var
+     */
     private   $client_secret;
 
+    /**
+     * OAuth Client ID
+     * @var
+     */
     private   $client_id;
 
 
-    //private   $socialite;
-
-
-	public function componentDetails() {
+	public function componentDetails()
+    {
 		return [
 			'name' => 'SocialLogin',
 			'description' => 'Allow users to login with 3th Party Accounts',
 		];
 	}
 
-	public function defineProperties() {
+	public function defineProperties()
+    {
 		return [];
 	}
-
 
 	public function onRun()
     {
@@ -67,10 +93,10 @@ class SocialLogin extends ComponentBase {
         //Authorize user if Request has code
         if(Request::has('code')){
 
-            if(!$this->getSession())
+            if(!$credentials = $this->getSession())
                 return;
 
-            $credentials = $this->getSession();
+            //reuse saved session
             $this->provider = $credentials['provider'];
             $this->client_id = $credentials['client_id'];
             $this->callback_url = $credentials['callback_url'];
@@ -79,12 +105,16 @@ class SocialLogin extends ComponentBase {
             $this->request = $this->createRequest($this->provider);
 
             dd($this->request->user());
-        //reuse save session
 
         }
 
 	}
 
+    /**
+     * Initiate Socialite Provider
+     * @param $provider
+     * @return mixed
+     */
     public function createRequest($provider)
     {
         $instance = Socialite::driver($provider);
@@ -93,6 +123,9 @@ class SocialLogin extends ComponentBase {
         return $init;
     }
 
+    /**
+     * save OAuth credentials in a session array
+     */
     public function setSessionProvider()
     {
         if(Session::has('oauth'))
@@ -110,11 +143,19 @@ class SocialLogin extends ComponentBase {
 
     }
 
+    /*
+     * Get OAuth Session
+     */
     public function getSession()
     {
         return Session::get('oauth');
     }
 
+    /**
+     * Inject fetched Credentials in Socialite Object
+     * @param $instance
+     * @return mixed
+     */
     public function injectCredentials($instance){
 
         $instance = new $instance
@@ -139,15 +180,22 @@ class SocialLogin extends ComponentBase {
         return Auth::getUser();
     }
 
+    /**
+     * Get all registered Providers by Plugin User
+     * @return mixed
+     */
     public function providersList()
     {
         return Setting::lists('provider');
     }
 
+    /**
+     * Fetch from Database Credentials on Provider
+     * @param $provider
+     */
     public function providerData($provider)
     {
         $credential = Setting::where('provider', $provider)->first()->toArray();
-
         if($credential){
             $this->client_id = $credential['client_id'];
             $this->client_secret = $credential['client_secret'];
